e1bfb4d2f6c9e7e46e0c24c8d77542254fa74e79
LooperSalty <paulruiz944@gmail.com>
2023-10-18 15:07:13 +0200

    bernarddd

--- cache.c
+++ cache.c
@@ -10,25 +10,36 @@ int nbrcacher(bool *victoire) {
     int n, score, nbrutilisateurs, tentatives = 0;
     bool correct = false;
 
-    *victoire = false;
-
     srand(time(NULL));
 
+     const char *phrases[5] = {
+        "lâche pas les études",
+        "toujours aussi nul",
+        "encore sa fait beaucoup la non ?",
+        "aie coup du pour guillaume",
+        "T'est éclater au sol !"
+    };
+
+    *victoire = false;
     n = rand() % 1000 + 1; 
 
     printf("Devinez un nombre entre 1 et 1000\n");
 
     while (tentatives < 10) { 
+        int indice = rand() % 5;
         printf("Tentative %d : ", ++tentatives);
         scanf("%d", &nbrutilisateurs); 
 
         // Verification du nombre
         if (nbrutilisateurs < 1 || nbrutilisateurs > 1000) {
             printf("Le nombre doit être entre 1 et 1000. Réessayez.\n");
+            printf("%s\n", phrases[indice]);
         } else if (nbrutilisateurs < n) {
             printf("Le nombre à deviner est plus grand.\n");
+            printf("%s\n", phrases[indice]);
         } else if (nbrutilisateurs > n) {
             printf("Le nombre à deviner est plus petit.\n");
+            printf("%s\n", phrases[indice]);
         } else {
             correct = *victoire = true;
             break; 
--- main.c
+++ main.c
@@ -3,36 +3,80 @@
 #include "sae.h"
 
 static void jeu_gagne(int score) {
-    printf("Gagne avec un score de %d\n", score);
+    printf("\n");
+    printf("  ____                               _ \n");
+    printf(" / ___| __ _  __ _ _ __   ___ _ __  | |\n");
+    printf("| |  _ / _` |/ _` | '_ \\ / _ \\ '__| | |\n");
+    printf("| |_| | (_| | (_| | | | |  __/ |    |_|\n");
+    printf(" \\____|\\__,_|\\__, |_| |_|\\___|_|    (_)\n");
+    printf("             |___/\n");
+    printf("\n");
+    printf("\033[1;32m\n avec un score de %d\n033[0m", score);
 }
+
 static void jeu_perdu(int score) {
-    printf("Aie... Coup dur... Score: %d\n", score);
+    printf("\n");
+    printf("                    _         _ \n");
+    printf(" _ __   ___ _ __ __| |_   _  | |\n");
+    printf("| '_ \\ / _ \\ '__/ _` | | | | | |\n");
+    printf("| |_) |  __/ | | (_| | |_| | |_|\n");
+    printf("| .__/ \\___|_|  \\__,_|\\__,_| (_)\n");
+    printf("|_|                              \n");
+    printf("\n");
+    printf("\033[1;31m\nAie... Coup dur... Score: %d\n\033[0m", score);
 }
 
 int main() {
-    puts("NOTRE SUPER JEU!!!1!\n"
-         " 1 - Suite mystere\n"
-         " 2 - Nombre cache\n"
-         " 3 - Mastermind");
+    bool rejouer;
+    printf("\n");
+    printf("  _     _                                      \n");
+    printf(" | |__ (_) ___ _ ____   _____ _ __  _   _  ___ \n");
+    printf(" | '_ \\| |/ _ \\ '_ \\ \\ / / _ \\ '_ \\| | | |/ _ \\\n");
+    printf(" | |_) | |  __/ | | \\ V /  __/ | | | |_| |  __/\n");
+    printf(" |_.__/|_|\\___|_| |_|\_/ \\___|_| |_|\\__,_|\\___|\n");
+    printf("\n");
+    do {
+        puts("NOTRE SUPER JEU!!!1!\n"
+             " 1 - Suite mystere\n"
+             " 2 - Nombre cache\n"
+             " 3 - Mastermind");
 
-    int numero_jeu = 0;
-    while (numero_jeu < 1 || numero_jeu > 3) {
-        printf("Entrer le numero du jeu [1-3]: ");
-        scanf("%d", &numero_jeu);
-    }
-    while (getchar() != '\n');
+        int numero_jeu = 0;
+        while (numero_jeu < 1 || numero_jeu > 3) {
+            printf("Entrer le numero du jeu [1-3]: ");
+            scanf("%d", &numero_jeu);
+        }
+        while (getchar() != '\n');
 
-    int score; bool victoire;
-    switch (numero_jeu) {
-    case 1: score = suite_mystere(&victoire); break;
-    case 2: score = nbrcacher(&victoire); break;
-    case 3: score = mastermind(&victoire); break;
-    }
+        int score;
+        bool victoire;
 
-    if (victoire)
-        jeu_gagne(score);
-    else
-        jeu_perdu(score);
+        switch (numero_jeu) {
+            case 1: score = suite_mystere(&victoire); break;
+            case 2: score = nbrcacher(&victoire); break;
+            case 3: score = mastermind(&victoire); break;
+        }
+
+        if (victoire)
+            jeu_gagne(score);
+        else
+            jeu_perdu(score);
+
+        char choix;
+        do {
+            printf("Voulez-vous rejouer ? (o/n)\n");
+            while (getchar() != '\n');
+            scanf("%c", &choix);
+            if (choix == 'o') {
+                printf("C'est parti, on rejoue !\n");
+                rejouer = true;
+            } else if (choix == 'n') {
+                rejouer = false;
+            } else {
+                printf("Saisie invalide. Veuillez choisir 'o' pour rejouer ou 'n' pour ne pas rejouer.\n");
+            }
+        } while (choix != 'o' && choix != 'n');
+    } while (rejouer);
 
     return 0;
 }
