e4cd7e2df8464e781b20c95d60327fa43c6505a7
LooperSalty <paulruiz944@gmail.com>
2023-10-29 15:58:43 +0100

    tout marche sauf suite mystere

--- graphique/main.py
+++ graphique/main.py
@@ -1,18 +1,16 @@
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QTextEdit, QLineEdit
-from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtWidgets import QLineEdit, QApplication, QVBoxLayout, QWidget, QPushButton, QTextEdit
 import sys
-from cache import CacheGame
 from mastermind import mastermind_play
 from mystere import mystere
-
+from cache import CacheGame
 
 class GameWindow(QWidget):
     def __init__(self):
-        super().__init__()
-        self.initUI()
+      super().__init__()
+      self.initUI()
+      self.cache_game = None
 
-        self.cache_game = CacheGame()
+        
 
     def initUI(self):
         self.setWindowTitle("Jeux")
@@ -47,7 +45,7 @@ class GameWindow(QWidget):
             game_name = sender.text()
             self.result_text.clear()
             if game_name == "Nombre caché":
-                self.cache_game.tentatives = 0
+                self.cache_game = CacheGame()
                 self.result_text.append(f"Vous avez lancé le jeu : {game_name}")
                 self.result_text.append("Devinez un nombre entre 1 et 1000")
             elif game_name == "Mastermind":
@@ -56,16 +54,15 @@ class GameWindow(QWidget):
                 self.play_mystere()
             else:
                 self.result_text.append(f"Vous avez lancé le jeu : {game_name}")
-                
 
     def submit_number(self):
         user_input = self.input_field.text()
-        if user_input.isdigit():
+        if user_input.isdigit() and self.cache_game:
             number = int(user_input)
             result = self.cache_game.guess_number(number)
             self.result_text.append(result)
         else:
-            self.result_text.append("Veuillez entrer un nombre valide.")
+            self.result_text.append("Veuillez entrer un nombre valide ou sélectionner le jeu Nombre caché.")
 
     def play_mastermind(self):
         self.result_text.append("Vous avez lancé le jeu : Mastermind")
@@ -73,14 +70,13 @@ class GameWindow(QWidget):
         self.result_text.append("Les voyelles possibles sont : a, e, i, o, u, y.")
         self.result_text.append("Vous avez jusqu'à 10 essais pour retrouver la suite.")
 
-        tentatives = mastermind_play(self)
+        tentatives = mastermind_play()
 
         if tentatives <= 10:
             self.result_text.append(f"Bravo, vous avez trouvé la suite en {tentatives} essais!")
         else:
             self.result_text.append("Désolé, vous n'avez pas trouvé la suite en 10 essais.")
-            
-     
+
     def play_mystere(self):
         self.result_text.append("Vous avez lancé le jeu : Mystère")
         self.result_text.append("Vous devez résoudre le mystère de la suite de nombres.")
